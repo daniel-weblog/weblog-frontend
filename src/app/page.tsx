96e144dbdcc60f74fbcd4f420f7514e2f1be30cb
Daniel3178 <Daniel.ibrahimi360@gmail.com>
2025-08-05 16:20:27 +0200

    ci/cd refinement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1>Welcome to My Blog</h1>
-      <p>This is a simple blog built with Next.js</p>
+      <p>This is a simple blog</p>
     </div>
   );
 }
